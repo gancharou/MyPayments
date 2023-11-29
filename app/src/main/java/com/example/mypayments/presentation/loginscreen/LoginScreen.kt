2f200122644fd1d06e380e95524ee3049e6b54d2
Goncharov Nikolay <Goncharov@siberian.pro>
2023-11-29 10:20:35 +0400

    Разбил фрагмент на 2

--- app/src/main/java/com/example/mypayments/presentation/loginscreen/LoginScreen.kt
+++ app/src/main/java/com/example/mypayments/presentation/loginscreen/LoginScreen.kt
@@ -1,4 +1,4 @@
-package com.example.mypayments.presentation.authorizationscreen.widget
+package com.example.mypayments.presentation.loginscreen
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
